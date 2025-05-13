d7cf0a1aae9068872f969a548bdcd1691be376cd
Taufik_188 <taufik.123140188@student.itera.ac.id>
2025-05-13 14:18:19 +0700

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,3 +1,5 @@
+#screen.py
+
 import pygame
 
 class Camera:
@@ -21,10 +23,10 @@ def load_assets(width, height):
     
     # Load button images
     start_button_img = pygame.image.load("assets/image/start_button.png").convert_alpha()
-    start_button_img = pygame.transform.scale(start_button_img, (200, 50))  # Sesuaikan ukuran
+    start_button_img = pygame.transform.scale(start_button_img, (200, 100))  # Sesuaikan ukuran
     
-    exit_button_img = pygame.image.load("assets/image/start_button.png").convert_alpha()
-    exit_button_img = pygame.transform.scale(start_button_img, (200, 50))  # Ukuran sama dengan tombol start
+    exit_button_img = pygame.image.load("assets/image/exit_button.png").convert_alpha()
+    exit_button_img = pygame.transform.scale(exit_button_img, (200, 100))  # Ukuran sama dengan tombol start
     
     return background, platform_img, wall_img, start_button_img, exit_button_img
 
@@ -32,20 +34,14 @@ def draw_background(screen, background, camera_rect, screen_width, screen_height
     bg_sub = background.subsurface(camera_rect)
     bg_scaled = pygame.transform.scale(bg_sub, (screen_width, screen_height))
     screen.blit(bg_scaled, (0, 0))
-
 def draw_objects(screen, player, platforms, camera_rect, zoom):
-    for platform in platforms:
-        platform.scale_image(zoom)
-    player.scale_image(zoom)
-
+    # Gambar platform terlebih dahulu
     for platform in platforms:
         platform.draw(screen, camera_rect, zoom)
-
-    player_pos = (
-        (player.rect.x - camera_rect.x) * zoom,
-        (player.rect.y - camera_rect.y) * zoom
-    )
-    screen.blit(player.scaled_image, player_pos)
+    
+    # gambar pemain mengguanakan method draw milik player
+    player.draw(screen, camera_rect, zoom)
+  
 
 def draw_darkness_with_light(screen, player, camera_rect, zoom, light_radius=120):
     overlay = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
