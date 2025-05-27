04082ec3e6d42bf7b4c3f50b063203c4b35d3c3b
Taufik_188 <taufik.123140188@student.itera.ac.id>
2025-05-27 18:05:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from game import Game
+#main.py
+from game import Game # Make sure your main game class is in game.py
 
 if __name__ == "__main__":
-    Game().run()
+    game_instance = Game()
+    game_instance.run()
